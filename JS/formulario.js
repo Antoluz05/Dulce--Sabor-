fbc2a0ffd56c349bbf4df596b36b59747f8d0a72
antoluz2005 <antonellaluzsambueza@gmail.com>
2022-11-16 17:19:11 +0100

    Poniendo contenido al function

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,2 +1,8 @@
 const div = document.getElementById('div'); 
 function movimiento(){ 
+    const input = document.getElementById('input').value; 
+    const h1 = document.createElement('h4'); 
+    console.log(input); 
+    h1.textContent = input;
+     div.appendChild(h4); 
+     div.classList.add('rotacion');
